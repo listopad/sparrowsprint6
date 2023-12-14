694aad1284352aa32f21207779bbfd56b5c872ca
Artem <adelelain@gmail.com>
2023-12-14 12:35:30 +0300

    Initial Commit

---
+++ sparrowsprint6/sparrowsprint6App.swift
@@ -0,0 +1,17 @@
+//
+//  sparrowsprint6App.swift
+//  sparrowsprint6
+//
+//  Created by Artem Dragunov on 14.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct sparrowsprint6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
