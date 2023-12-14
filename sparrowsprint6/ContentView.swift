a12afa0039a80cf870b1ddc2ccdfa1cff9c17290
Artem <adelelain@gmail.com>
2023-12-14 15:12:52 +0300

    First try

--- sparrowsprint6/ContentView.swift
+++ sparrowsprint6/ContentView.swift
@@ -8,16 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State   var padding: CGFloat = 10
+    @State   var size: CGFloat = 100
+    @State   var rectCount: Int = 4
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        HStack {
+            ForEach(0..<rectCount, id: \.self) { _ in
+                self.coloredRectangle()
+            }
+        }.padding(padding)
     }
+    
+    func coloredRectangle() -> some View {
+        Rectangle()
+            .frame(width: calculateRectSize(), height: calculateRectSize())
+            .foregroundColor(randomColor())
+    }
+    
+    func randomColor() -> Color {
+        let colors: [Color] = [.blue, .red, .cyan, .mint, .purple]
+        return colors.randomElement() ?? .black
+    }
+    
+    
+    func calculateRectSize() -> CGFloat {
+        let screenWidth = UIScreen.main.bounds.width
+        let totalPadding = CGFloat(rectCount - 1) * padding
+        let rectWidth = (screenWidth - totalPadding) / CGFloat(rectCount)
+        
+        return rectWidth
+    }
+    
+    
 }
+
 
 #Preview {
     ContentView()
